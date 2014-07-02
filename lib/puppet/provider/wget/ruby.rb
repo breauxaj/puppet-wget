a328395956f8f3ab656482ddb7f9759dbbcdeb4e
Adam Breaux <breauxaj@gmail.com>
2014-07-02 18:09:06 -0400

    turn off debug messages

--- lib/puppet/provider/wget/ruby.rb
+++ lib/puppet/provider/wget/ruby.rb
@@ -8,7 +8,7 @@ Puppet::Type.type(:wget).provide(:ruby) do
   def create
     if File.directory? @resource[:path]
       (@resource[:source]).each do |c|
-        puts "source is " + c
+        #puts "source is " + c
         wget '-nc', c, '-P', @resource[:path]
       end
     end
@@ -17,7 +17,7 @@ Puppet::Type.type(:wget).provide(:ruby) do
   def destroy
     (@resource[:source]).each do |d|
       file = d.split("/").last
-      puts "destroy file is " + @resource[:path] + file
+      #puts "destroy file is " + @resource[:path] + file
       FileUtils.rm_f(@resource[:path] + '/' + file)
     end
   end
@@ -26,7 +26,7 @@ Puppet::Type.type(:wget).provide(:ruby) do
     flag = true
     (@resource[:source]).each do |e|
       file = e.split("/").last
-      puts "exists file is " + @resource[:path] + '/' + file
+      #puts "exists file is " + @resource[:path] + '/' + file
       if ! File.exists?(@resource[:path] + '/' + file)
         flag = false
       end
