d806734633a78a99c95ff748aa727793fe91485b
Adam Breaux <breauxaj@gmail.com>
2014-07-02 17:40:38 -0400

    better messages

--- lib/puppet/provider/wget/ruby.rb
+++ lib/puppet/provider/wget/ruby.rb
@@ -16,16 +16,16 @@ Puppet::Type.type(:wget).provide(:ruby) do
 
   def destroy
     (@resource[:source]).each do |s|
-      puts "source is " + s
       file = s.split("/").last
+      puts "destroy file is " + file
       FileUtils.rm_f(@resource[:path] + file)
     end
   end
 
   def exists?
     (@resource[:source]).each do |s|
-      puts "source is " + s
       file = s.split("/").last
+      puts "exists file is " + s
       File.exists?(@resource[:path] + file)
     end
   end
