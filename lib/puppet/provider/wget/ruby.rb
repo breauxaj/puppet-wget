d6a3d3e6ed5d0c154b9cefd71649567ef5072549
Adam Breaux <breauxaj@gmail.com>
2014-07-02 10:43:28 -0400

    not clear
    not that it ever is

--- lib/puppet/provider/wget/ruby.rb
+++ lib/puppet/provider/wget/ruby.rb
@@ -6,22 +6,22 @@ Puppet::Type.type(:wget).provide(:ruby) do
   commands :wget => 'wget'
 
   def create
-    if File.directory? resource[:name]
-      puts resource[:source], resource[:name]
-      wget resource[:source], '-P', resource[:name]
+    if File.directory? resource[:path]
+      puts resource[:source], resource[:path]
+      wget resource[:source], '-P', resource[:path]
     end
   end
 
   def destroy
     file = resource[:source].split("/").last
-    puts resource[:name] + '/' + file
-    FileUtils.rm_f resource[:name] + '/' + file
+    puts resource[:path] + file
+    FileUtils.rm_f resource[:path] + file
   end
 
   def exists?
     file = resource[:source].split("/").last
-    puts resource[:name] + '/' + file
-    File.exists? resource[:name] + '/' + file
+    puts resource[:path] + file
+    File.exists? resource[:path] + file
   end
   
 end
