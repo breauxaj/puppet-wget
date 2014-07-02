6c46234328a23037f7f68e03885e0cdef161b55a
Adam Breaux <breauxaj@gmail.com>
2014-07-02 10:18:38 -0400

    echo something

--- lib/puppet/provider/wget/ruby.rb
+++ lib/puppet/provider/wget/ruby.rb
@@ -6,16 +6,19 @@ Puppet::Type.type(:wget).provide(:ruby) do
   commands :wget => 'wget'
 
   def create
+    puts resource[:source]
     wget resource[:source], '-P', resource[:name]
   end
 
   def destroy
     file = resource[:source].split("/").last
+    puts file
     FileUtils.rm_f resource[:name] + '/' + file
   end
 
   def exists?
     file = resource[:source].split("/").last
+    puts file
     File.exists? resource[:name] + '/' + file
   end
   
