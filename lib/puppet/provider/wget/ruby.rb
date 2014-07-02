7c231b2cc04b464b89dd2ab2212a1a34dd528963
Adam Breaux <breauxaj@gmail.com>
2014-07-02 17:46:57 -0400

    minor tweaks

--- lib/puppet/provider/wget/ruby.rb
+++ lib/puppet/provider/wget/ruby.rb
@@ -17,16 +17,16 @@ Puppet::Type.type(:wget).provide(:ruby) do
   def destroy
     (@resource[:source]).each do |d|
       file = d.split("/").last
-      puts "destroy file is " + file
-      FileUtils.rm_f(@resource[:path] + file)
+      puts "destroy file is " + @resource[:path] + file
+      FileUtils.rm_f(@resource[:path] + '/' + file)
     end
   end
 
   def exists?
     (@resource[:source]).each do |e|
       file = e.split("/").last
-      puts "exists file is " + file
-      File.exists?(@resource[:path] + file)
+      puts "exists file is " + @resource[:path] + file
+      File.exists?(@resource[:path] + '/' + file)
     end
   end
 
