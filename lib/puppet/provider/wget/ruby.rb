86b5bc002a4751e2173d0709647ea909e8da013c
Adam Breaux <breauxaj@gmail.com>
2014-07-02 11:12:25 -0400

    is it the if?

--- lib/puppet/provider/wget/ruby.rb
+++ lib/puppet/provider/wget/ruby.rb
@@ -6,21 +6,23 @@ Puppet::Type.type(:wget).provide(:ruby) do
   commands :wget => 'wget'
 
   def create
-    if File.directory? resource[:path]
-      puts resource[:source], resource[:path]
-      wget resource[:source], '-P', resource[:path]
+    if File.directory? @resource[:path]
+      puts @resource[:source], @resource[:path]
+      wget @resource[:source], '-P', @resource[:path]
+    else
+      puts 'create failed'
     end
   end
 
   def destroy
-    file = resource[:source].split("/").last
-    puts 'destroy ' + resource[:path] + file
+    file = @resource[:source].split("/").last
+    puts 'destroy ' + @resource[:path] + file
     FileUtils.rm_f(@resource[:path] + file)
   end
 
   def exists?
-    file = resource[:source].split("/").last
-    #puts 'exists? ' + resource[:path] + file
+    file = @resource[:source].split("/").last
+    #puts 'exists? ' + @resource[:path] + file
     File.exists?(@resource[:path] + file)
   end
   
